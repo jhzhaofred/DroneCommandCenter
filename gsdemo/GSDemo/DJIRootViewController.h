5ef5cf9fa7f00c39062841de2b7bdb9478b06caf
caoliyi <31828844+caoliyi@users.noreply.github.com>
2018-04-10 15:47:31 -0400

    Add files via upload
    should use pod install and DJIAppKey

---
+++ gsdemo/GSDemo/DJIRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  DJIRootViewController.h
+//  GSDemo
+//
+//  Created by DJI on 7/7/15.
+//  Copyright (c) 2015 DJI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DJIRootViewController : UIViewController
+
+@end
